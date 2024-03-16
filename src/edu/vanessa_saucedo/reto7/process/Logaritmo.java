92521c0a328133c4056ead4daea7f7e14b9133ec
Vanessa Saucedo <vaneflakabts@gmail.com>
2024-03-15 20:13:14 -0600

    Logaritmo con comentario faltante

--- src/edu/vanessa_saucedo/reto7/process/Logaritmo.java
+++ src/edu/vanessa_saucedo/reto7/process/Logaritmo.java
@@ -4,6 +4,11 @@ package edu.vanessa_saucedo.reto7.process;
  * Clase especial para calcular el Logaritmo
  */
 public class Logaritmo extends OperacionAritmetica{
+
+    /**
+     * Metodo extendido de la clase division y operacion aritmetica
+     * para calcular el logaritmo
+     */
     @Override
     public double calcular(double num1, double num2) {
         double logaritmoNumero = Math.log(num1);
